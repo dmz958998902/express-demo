5ca05026aa3cb25f13c3a6599416f074116cf67c
Your Name <you@example.com>
2019-09-19 22:03:28 +0800

    async和await，密码加密

--- rout/user.js
+++ rout/user.js
@@ -6,9 +6,9 @@ router.get("/register",(req,res)=>{
    res.render("register");
 });
 //注册路由的一系列操作
-router.post("/store",(req,res)=>{
+router.post("/store",async(req,res)=>{
     //获取注册过程中form表单的数据
-    console.log(req.body);
+    // console.log(req.body);
     let username=req.body.username;
     let email=req.body.email;
     let password=req.body.password;
@@ -17,23 +17,37 @@ router.post("/store",(req,res)=>{
         res.send("参数有误");
         return;
     }
-    UserModel.findOne({email:req.body.email}).then(dat=>{
-        console.log(dat)
-        if(dat){
-            res.send("已经注册过了")
-        }else{
- //将数据存到数据库中
-    const data=new UserModel({
-        username:username,
-        email:email,
-        password:password
-    });
-    data.save().then(()=>{
-        res.send("注册成功")
-    }).catch(()=>{
-        res.send("注册失败");
-    })
-        }
-    })
+    let data= await UserModel.findOne({email:req.body.email});
+    console.log(data)
+    if(data){
+        //已经注册过了
+        res.send("已经注册过了")
+    }else{
+        let reg=new UserModel({
+            username:req.body.username,
+            email:req.body.email,
+            password:bcryptjs.hashSync(req.body.password)
+        });
+        await reg.save();
+        res.send("注册成功");
+    }
+//     UserModel.findOne({email:req.body.email}).then(dat=>{
+//         console.log(dat)
+//         if(dat){
+//             res.send("已经注册过了")
+//         }else{
+//  //将数据存到数据库中
+//     const data=new UserModel({
+//         username:username,
+//         email:email,
+//         password:password
+//     });
+//     data.save().then(()=>{
+//         res.send("注册成功")
+//     }).catch(()=>{
+//         res.send("注册失败");
+//     })
+//         }
+//     })
 })
 module.exports=router
